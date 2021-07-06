f09d5cbdccd92ef09675152ec79d1f51420bff23
ThangNguyenTan <nguyentanthang1118@gmail.com>
2021-07-06 08:31:29 +0700

    Version 0.9.5 - Added API unit testing for Categories route

--- tests/Feature/AuthorApiTest.php
+++ tests/Feature/AuthorApiTest.php
@@ -6,9 +6,30 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
+$createdAuthorID = 1;
+
 class AuthorApiTest extends TestCase
 {
     private $url = '/api/authors';
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_can_create_author()
+    {
+        GLOBAL $createdAuthorID;
+
+        $data = [
+            "author_name" => "Marcel Proust",
+            "author_bio" => "It's nice to be nice"
+        ];
+        $response = $this->post("$this->url", $data);
+        $createdAuthorID = $response->original['id'];
+
+        $response->assertStatus(200);
+    }
 
     /**
      * A basic feature test example.
@@ -25,11 +46,14 @@ class AuthorApiTest extends TestCase
     /**
      * A basic feature test example.
      *
+     * @depends test_can_create_author
      * @return void
      */
     public function test_can_get_author_by_id()
     {
-        $response = $this->get("$this->url/2");
+        GLOBAL $createdAuthorID;
+
+        $response = $this->get("$this->url/$createdAuthorID");
 
         $response->assertStatus(200);
     }
@@ -49,31 +73,18 @@ class AuthorApiTest extends TestCase
     /**
      * A basic feature test example.
      *
-     * @return void
-     */
-    public function test_can_create_author()
-    {
-        $data = [
-            "author_name" => "Marcel Proust",
-            "author_bio" => "It's nice to be nice"
-        ];
-        $response = $this->post("$this->url", $data);
-
-        $response->assertStatus(200);
-    }
-
-    /**
-     * A basic feature test example.
-     *
+     * @depends test_can_create_author
      * @return void
      */
     public function test_can_update_author()
     {
+        GLOBAL $createdAuthorID;
+
         $data = [
             "author_name" => "Steven King",
             "author_bio" => "I am the king of horror"
         ];
-        $response = $this->put("$this->url/1", $data);
+        $response = $this->put("$this->url/$createdAuthorID", $data);
 
         $response->assertStatus(200);
     }
@@ -81,11 +92,14 @@ class AuthorApiTest extends TestCase
     /**
      * A basic feature test example.
      *
+     * @depends test_can_create_author
      * @return void
      */
     public function test_can_delete_author()
     {
-        $response = $this->delete("$this->url/12");
+        GLOBAL $createdAuthorID;
+
+        $response = $this->delete("$this->url/$createdAuthorID");
 
         $response->assertStatus(200);
     }
--- tests/Feature/CategoryApiTest.php
+++ tests/Feature/CategoryApiTest.php
@@ -6,16 +6,100 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
+$createdCategoryID = 1;
+
 class CategoryApiTest extends TestCase
 {
+    private $url = '/api/categories';
+
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function test_example()
+    public function test_can_create_category()
     {
-        $response = $this->get('/');
+        GLOBAL $createdCategoryID;
+
+        $data = [
+            "category_name" => "Action",
+            "category_desc" => "It's nice to be nice"
+        ];
+        $response = $this->post("$this->url", $data);
+        $createdCategoryID = $response->original['id'];
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_can_get_all_categories()
+    {
+        $response = $this->get("$this->url");
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @depends test_can_create_category
+     * @return void
+     */
+    public function test_can_get_category_by_id()
+    {
+        GLOBAL $createdCategoryID;
+
+        $response = $this->get("$this->url/$createdCategoryID");
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_cannot_get_category_by_id()
+    {
+        $response = $this->get("$this->url/123");
+
+        $response->assertStatus(404);
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @depends test_can_create_category
+     * @return void
+     */
+    public function test_can_update_category()
+    {
+        GLOBAL $createdCategoryID;
+
+        $data = [
+            "category_name" => "Horror",
+            "category_desc" => "Man I am scared"
+        ];
+        $response = $this->put("$this->url/$createdCategoryID", $data);
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @depends test_can_create_category
+     * @return void
+     */
+    public function test_can_delete_category()
+    {
+        GLOBAL $createdCategoryID;
+
+        $response = $this->delete("$this->url/$createdCategoryID");
 
         $response->assertStatus(200);
     }
