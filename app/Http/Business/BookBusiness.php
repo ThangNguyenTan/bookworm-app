d1fc784de0903181467728dc151cf612158833ed
ThangNguyenTan <nguyentanthang1118@gmail.com>
2021-07-26 17:20:29 +0700

    Version 1.0.0 - Up and ready for demo

--- app/Http/Business/BookBusiness.php
+++ app/Http/Business/BookBusiness.php
@@ -50,13 +50,33 @@ class BookBusiness
         return $books;
     }
 
+    public function fetchRequiredFieldsForOnSaleBooks() {
+        $utils = new Utilities();
+
+        $array = [
+            "books.*",
+            "authors.id AS author_id", 
+            "authors.author_name AS author_name", 
+            "$utils->min_discount_price_query_coalesce AS discount_price"
+        ];
+        $comma_separated = implode(",", $array);
+
+        $books = DB::table("books")
+        ->join("discounts", "books.id", "=", "discounts.book_id")
+        ->join("authors", "books.author_id", "=", "authors.id")
+        ->selectRaw($comma_separated)
+        ->groupBy("books.id", "authors.id");
+
+        return $books;
+    }
+
     public function getOnSaleBooks($limit){
         $sorter = new Sorter();
 
         // Filter the books by on sale
         // i.e. the difference between book price and discount price. 
         // The higher the difference the higher the rankings
-        $onSaleBooks = $this->fetchRequiredFieldsForHome();
+        $onSaleBooks = $this->fetchRequiredFieldsForOnSaleBooks();
         $onSaleBooks = $sorter->sortBooksQuery($onSaleBooks, "onsale");
         $onSaleBooks = $onSaleBooks
         ->skip(0)
